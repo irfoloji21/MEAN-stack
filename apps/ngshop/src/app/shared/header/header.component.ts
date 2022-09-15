473ef2f237471aa48f678483e2fe593f7c685db6
İrfan MEŞE <softwaredevelopment2181@gmail.com>
2022-09-15 19:46:57 +0300

    category

---
+++ apps/ngshop/src/app/shared/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngshop-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit {
+  constructor() {}
+
+  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
+  ngOnInit(): void {}
+}
